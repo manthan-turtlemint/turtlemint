a51f3c0481cc47af0b74a09448052276a70374aa
Manthan <manthan@Manthans-MacBook-Pro.local>
2026-02-17 00:22:12 +0530

    Fix: allow access to auth error page in middleware

--- middleware.ts
+++ middleware.ts
@@ -42,6 +42,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * - login (login page)
          */
-        "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|login|auth).*)",
     ],
 };
